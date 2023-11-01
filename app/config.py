2e2c45bfc795b127e15945b8127d9133f58ed51f
oSumAtrIX <johan.melkonyan1@web.de>
2023-11-01 19:38:50 +0100

    refactor: Move configuration loading to `load_config()`

--- app/config.py
+++ app/config.py
@@ -3,4 +3,7 @@ import json
 
 def load_config() -> dict:
     with open("config.json", "r") as config_file:
-        return json.load(config_file)
+        config = json.load(config_file)
+        config["output"] = config["output"] if "output" in config else "static"
+        config["purge"] = config["purge"] if "purge" in config else []
+
--- main.py
+++ main.py
@@ -6,21 +6,19 @@ from app.generator import DefaultGeneratorProvider
 
 config = load_config()
 
-output = config["output"] if "output" in config else "static"
-purge = config["purge"] if "purge" in config else []
-generator_configs = config["configs"]
-
 generator_provider = DefaultGeneratorProvider()
 
-for path in purge:
+for path in config["purge"]:
     if isdir(path):
         shutil.rmtree(path)
     elif isfile(path):
         os.remove(path)
 
-for config in generator_configs:
-    for generator_name in config["generators"]:
+output = config["output"]
+
+for generator_config in config["configs"]:
+    for generator_name in generator_config["generators"]:
         generator = generator_provider.get(generator_name)
         if generator is None:
             continue
-        generator.generate(config, output)
+        generator.generate(generator_config, output)
